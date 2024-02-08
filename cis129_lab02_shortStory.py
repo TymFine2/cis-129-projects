a4d1214a92a8969a07b21de6c4085d0da2ca5254
TymFine2 <159149300+TymFine2@users.noreply.github.com>
2024-02-07 23:51:56 -0700

    Update cis129_lab02_shortStory.py
    I added print statements for four sentences, after creating comments about what they were going to be.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+## These are comments, to explain this code,that also explain the comments.
+## ..it's basically self-aware.
+##This program is an example of comments and the print function
+print('This is for the module 2 Lab.')
+print('I just need to make a few sentences with the print function.')
+print('I will also need to make comments that acknowlege and explain my code.')
+print('This may just be my biggest contribution to github yet.')
